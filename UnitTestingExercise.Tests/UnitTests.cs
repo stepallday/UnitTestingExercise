78de0248247a8446ccae1420fdbaa3cfd3f39b29
Stephanie Hicks <stepallday@yahoo.com>
2020-12-02 09:49:04 -0500

    completedUnitTestingExercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,61 +6,76 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3,6)]
+        [InlineData(5,5,5,15)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Add(num1, num2, num3);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5,5,0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Subtract (minuend, subtrhend);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2,2,4)]
+        [InlineData(4,5,20)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Multiply (num1  , num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(20,5,4)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Divide (num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void YourMethodName()
         {
             //Arrange
+            var expected = 'h';
 
             //Act
+            var actual = 'h';
 
             //Assert
+            Assert.Equal(expected,actual);
 
         }
 
